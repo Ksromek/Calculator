6b2286560d3fc86a31ef29053d49913779c4445a
Kamil <ksromek10@gmail.com>
2023-05-21 16:33:21 +0200

    Commit II

---
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -0,0 +1,27 @@
+package com.example.calculator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CalculatorController {
+
+    @FXML
+    public void plus()
+    {
+        System.out.println("plus");
+    }
+
+    @FXML
+    public void minus()
+    {
+        System.out.println("minus");
+    }
+
+    @FXML
+    public void equal()
+    {
+        System.out.println("equal");
+    }
+
+
+}
