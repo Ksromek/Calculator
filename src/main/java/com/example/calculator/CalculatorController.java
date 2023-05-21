7253d11dfbb5011ca439a9b3f839b748b5f2d4c3
Kamil <ksromek10@gmail.com>
2023-05-21 16:51:08 +0200

    Commit II

--- src/main/java/com/example/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -1,27 +1,12 @@
 package com.example.calculator;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class CalculatorController {
 
     @FXML
     public void plus()
     {
-        System.out.println("plus");
+        System.out.println("Hello");
     }
-
-    @FXML
-    public void minus()
-    {
-        System.out.println("minus");
-    }
-
-    @FXML
-    public void equal()
-    {
-        System.out.println("equal");
-    }
-
-
 }
