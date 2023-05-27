d650535e15a7bed974b0be3c2acd6e65eebd3ddb
Kamil <ksromek10@gmail.com>
2023-05-27 13:29:59 +0200

    Commit III

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -2,22 +2,20 @@ package com.example.calculator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-
 public class CalculatorApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApplication.class.getResource("calculator-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 300, 500);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("calculator-view.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
+
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/com/example/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/CalculatorController.java
@@ -1,12 +1,146 @@
 package com.example.calculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 
 public class CalculatorController {
 
     @FXML
-    public void multiple()
-    {
-        System.out.println("Hello");
+    private TextField textField;
+
+    @FXML
+    private Text savedNumbers;
+
+    private String firstNumber = "";
+
+    private String currentNumber = "";
+
+    private String calculationType;
+
+    @FXML
+    void addition(ActionEvent event) {
+        calculationSetup("+");
+    }
+
+    @FXML
+    void substraction(ActionEvent event) {
+        calculationSetup("-");
+    }
+
+    @FXML
+    void division(ActionEvent event) {
+        calculationSetup("/");
+    }
+
+    @FXML
+    void multiplication(ActionEvent event) {
+        calculationSetup("*");
+    }
+
+    public void calculationSetup(String calculationType){
+        this.calculationType = calculationType;
+        firstNumber = currentNumber;
+        currentNumber = "";
+        savedNumbers.setText(firstNumber + " " + calculationType);
+    }
+
+    @FXML
+    void equals(ActionEvent event) {
+        int firstNumberInt = Integer.parseInt(firstNumber);
+        int secondNumberInt = Integer.parseInt(currentNumber);
+
+        switch (calculationType) {
+            case "+" -> {
+                int calculatedNumber = firstNumberInt + secondNumberInt;
+                savedNumbers.setText(firstNumber + " + " + currentNumber + " = " + calculatedNumber);
+                textField.setText(String.valueOf(calculatedNumber));
+            }
+            case "-" -> {
+                int calculatedNumber = firstNumberInt - secondNumberInt;
+                savedNumbers.setText(firstNumber + " - " + currentNumber + " = " + calculatedNumber);
+                textField.setText(String.valueOf(calculatedNumber));
+            }
+            case "/" -> {
+                double calculatedNumber = firstNumberInt / (double)secondNumberInt;
+                savedNumbers.setText(firstNumber + " / " + currentNumber + " = " + calculatedNumber);
+                textField.setText(String.valueOf(calculatedNumber));
+            }
+            case "*" -> {
+                int calculatedNumber = firstNumberInt * secondNumberInt;
+                savedNumbers.setText(firstNumber + " * " + currentNumber + " = " + calculatedNumber);
+                textField.setText(String.valueOf(calculatedNumber));
+            }
+        }
+    }
+
+    @FXML
+    void clear(ActionEvent event) {
+        currentNumber = "";
+        textField.setText("");
+        savedNumbers.setText("");
+    }
+
+    @FXML
+    void zero(ActionEvent event) {
+        if(!currentNumber.equals("")){
+            addNumber("0");
+        }
+    }
+
+    @FXML
+    void one(ActionEvent event) {
+        addNumber("1");
+    }
+
+    @FXML
+    void two(ActionEvent event) {
+        addNumber("2");
+    }
+
+    @FXML
+    void three(ActionEvent event) {
+        addNumber("3");
+    }
+
+    @FXML
+    void four(ActionEvent event) {
+        addNumber("4");
+    }
+
+    @FXML
+    void five(ActionEvent event) {
+        addNumber("5");
+    }
+
+    @FXML
+    void six(ActionEvent event) {
+        addNumber("6");
+    }
+
+    @FXML
+    void seven(ActionEvent event) {
+        addNumber("7");
+    }
+
+    @FXML
+    void eight(ActionEvent event) {
+        addNumber("8");
+    }
+
+    @FXML
+    void nine(ActionEvent event) {
+        addNumber("9");
+    }
+
+    public void updateTextField(){
+        textField.setText(currentNumber);
+    }
+
+    public void addNumber(String number){
+        currentNumber += number;
+        updateTextField();
     }
 }
